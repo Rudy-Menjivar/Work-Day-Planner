aa847aecf3be4e1439613086217bab669445a18c
Rudy-Menjivar <rudy.menjivar@gmail.com>
2020-11-04 06:03:26 -0800

    Getting currentHour from DateTime

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 
-// DateTime const for use with luxon
+// DateTime const for use with luxon DateTime
 const DateTime = luxon.DateTime;
 
 // Converting DateTime to LocaleString
@@ -8,5 +8,9 @@ console.log(todaY);
 
 // Applied today's date to currentDay id
 $("#currentDay").text(todaY);
+
+// Getting currentHour from DateTime
+var currentHour = DateTime.local().hour;
+console.log(currentHour);
 
 // DateTime.local().toFormat("HH 'hours and' mm 'minutes'"); //=> '20 hours and 55 minutes'
